4bea3f40b498157b9064433ccb88aab3d0921dab
Blynqs <baptiste.friboulet@epitech.eu>
2023-06-18 21:01:03 +0100

    [BF-ADD] Setup Vue-CLI Vuex TypeScript & Stylus

--- front/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div id="app">
-    <h1>TidyBee</h1>
-    <router-view></router-view>
-  </div>
-</template>
--- front/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-
-new Vue({
-    router, // Add the router instance to the Vue app
-    render: (h) => h(App),
-}).$mount('#app');
