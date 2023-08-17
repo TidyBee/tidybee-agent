4363f12ae463f8a60ebf40582c015660f19802dd
majent4 <99695392+majent4@users.noreply.github.com>
2023-08-17 13:24:24 +0200

    watcher: init notify implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    let path = std::env::args()
+        .nth(1)
+        .expect("Argument 1 needs to be a path");
+    log::info!("Watching {path}");
+    if let Err(error) = watch(path) {
+        log::error!("Error: {error:?}");
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+    for res in rx {
+        match res {
+            Ok(event) => log::info!("Change: {event:?}"),
+            Err(error) => log::error!("Error: {error:?}"),
+        }
+    }
+
+    Ok(())
 }
