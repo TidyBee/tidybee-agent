0192c9a12a571280d393e764f84bdd9ad0650fb0
majent4 <99695392+majent4@users.noreply.github.com>
2023-08-17 17:07:43 +0200

    add init cli error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
-use log::info;
-use std::env;
-
 mod watcher;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect::<Vec<String>>();
+    let num_args: usize = args.len();
+
+    if num_args < 2 {
+        print_help();
+        std::process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "--watch" => {
+            if num_args < 3 {
+                print_help();
+                std::process::exit(1);
+            }
+        }
+        "--help" => {
+            if num_args == 2 {
+                print_help();
+                std::process::exit(0);
+            } else {
+                print_help();
+                std::process::exit(1);
+            }
+        }
+        _ => {
+            print_help();
+            std::process::exit(1);
+        }
+    }
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let file_to_watch_path = env::args().nth(1).expect("Argument 1 needs to be a path");
+    let file_to_watch_path = std::env::args().nth(2)
+        .expect("Argument needs to be a path");
 
-    info!("Watching {}", file_to_watch_path);
     if let Err(error) = watcher::watch(&file_to_watch_path) {
         log::error!("Error: {:?}", error);
     }
+}
+
+fn print_help() {
+    eprintln!("Usage:
+      --watch <directory> : Watch for changes in the specified directory
+      --help              : Print help message and exit 0");
 }
--- src/watcher.rs
+++ src/watcher.rs
@@ -5,10 +5,15 @@ pub fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
     let (tx, rx) = std::sync::mpsc::channel();
     let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
     watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
     for res in rx {
         match res {
-            Ok(event) => log::info!("Change: {:?}", event),
-            Err(error) => log::error!("Error: {:?}", error),
+            Ok(event) => {
+                log::info!("Change: {:?}", event)
+            }
+            Err(error) => {
+                log::error!("Error: {:?}", error)
+            }
         }
     }
     Ok(())
