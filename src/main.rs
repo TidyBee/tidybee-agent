a5224c669538709ecdd64e7e087111102c8ecce5
majent4 <99695392+majent4@users.noreply.github.com>
2023-08-19 10:53:05 +0200

    options: parse w l e and t options

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,79 @@
-mod watcher;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect::<Vec<String>>();
-    let num_args: usize = args.len();
+    let matches = App::new("TidyBee")
+        .version("0.0.1")
+        .author("majent4")
+        .about("Watch for file changes and list directories")
+        .arg(
+            Arg::with_name("extension")
+                .short("e")
+                .long("extension")
+                .value_name("EXTENSIONS")
+                .multiple(true)
+                .use_delimiter(true)
+                .takes_value(true)
+                .help("Specify extensions"),
+        )
+        .arg(
+            Arg::with_name("type")
+                .short("t")
+                .long("type")
+                .value_name("TYPES")
+                .multiple(true)
+                .use_delimiter(true)
+                .takes_value(true)
+                .help("Specify types"),
+        )
+        .arg(
+            Arg::with_name("list")
+                .short("l")
+                .long("list")
+                .value_name("DIRECTORIES")
+                .multiple(true)
+                .use_delimiter(true)
+                .takes_value(true)
+                .required_unless("watch")
+                .conflicts_with("watch")
+                .help("Specify directories for listing"),
+        )
+        .arg(
+            Arg::with_name("watch")
+                .short("w")
+                .long("watch")
+                .value_name("DIRECTORIES")
+                .multiple(true)
+                .use_delimiter(true)
+                .required_unless("list")
+                .conflicts_with("list")
+                .takes_value(true)
+                .help("Specify directories for watching"),
+        )
+        .get_matches();
 
-    if num_args < 2 {
-        print_help();
+    if matches.is_present("list") && matches.is_present("watch") {
+        eprintln!("Error: -l and -w cannot be provided at the same time.");
         std::process::exit(1);
     }
 
-    match args[1].as_str() {
-        "--watch" => {
-            if num_args < 3 {
-                print_help();
-                std::process::exit(1);
-            }
-        }
-        "--help" => {
-            if num_args == 2 {
-                print_help();
-                std::process::exit(0);
-            } else {
-                print_help();
-                std::process::exit(1);
-            }
-        }
-        _ => {
-            print_help();
-            std::process::exit(1);
-        }
+    // Process the rest of the arguments as needed
+    if let Some(extensions) = matches.values_of("extension") {
+        let e: Vec<_> = extensions.collect();
+        println!("Extensions: {:?}", e);
     }
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let file_to_watch_path: String = std::env::args()
-        .nth(2)
-        .expect("Argument needs to be a path");
-
-    if let Err(error) = watcher::watch(&file_to_watch_path) {
-        log::error!("Error: {:?}", error);
+    if let Some(types) = matches.values_of("type") {
+        let t: Vec<_> = types.collect();
+        println!("Types: {:?}", t);
     }
-}
 
-fn print_help() {
-    eprintln!(
-        "Usage:
-  --watch <directory> : Watch for changes in the specified directory
-  --help              : Print help message and exit 0"
-    );
+    if let Some(directories) = matches.values_of("list") {
+        let d: Vec<_> = directories.collect();
+        println!("List Directories: {:?}", d);
+    }
+
+    if let Some(directories) = matches.values_of("watch") {
+        let d: Vec<_> = directories.collect();
+        println!("Watch Directories: {:?}", d);
+    }
 }
