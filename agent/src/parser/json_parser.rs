0b618dcbf838d964a9abf4e0894c50e9e26ffacb
Ju <julien.hourcadette@live.fr>
2023-09-09 12:41:42 +0200

    parser: json_parser must be deleted

--- agent/src/parser/json_parser.rs
+++ agent/src/parser/json_parser.rs
@@ -4,6 +4,7 @@ use json;
 use json::JsonValue;
 use std::error::Error;
 
+// Must be deleted
 pub fn read_value_from_file(path: &str, value: String) -> Result<JsonValue, Box<dyn Error>>{
     let file = File::open(path).unwrap();
     let string = file_into_string(file).unwrap();
