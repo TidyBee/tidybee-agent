b6f707f697fec00fa1dfad25be4c88efa08ff2ce
cvigneron <cvigneron@TeamCity>
2023-09-11 18:48:06 +0000

    TeamCity change in 'tidybee-hub' project: VCS roots of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -18,7 +18,6 @@ accordingly, and delete the patch script.
 changeBuildType(RelativeId("Build")) {
     vcs {
         remove(DslContext.settingsRoot.id!!)
-        add(RelativeId("HttpsGithubComTidyBeeTidybeeBackendRefsHeadsMain1"), "+:hub => .")
     }
 
     expectSteps {
