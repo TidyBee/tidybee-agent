fc69f4c4ed345b675aad270a877a40d8202805f4
majent4 <99695392+majent4@users.noreply.github.com>
2023-10-13 15:40:40 +0200

    file-man: add move_file function and easy tests

--- src/file_manager/mod.rs
+++ src/file_manager/mod.rs
@@ -1,8 +1,17 @@
 use std::fs;
 use std::io;
 
+#[allow(dead_code)]
 pub fn delete_file(file_path: &str) -> Result<(), io::Error> {
     match fs::remove_file(file_path) {
+        Ok(()) => Ok(()),
+        Err(err) => Err(err),
+    }
+}
+
+#[allow(dead_code)]
+pub fn move_file(source_file_path: &str, target_file_path: &str) -> Result<(), io::Error> {
+    match fs::rename(source_file_path, target_file_path) {
         Ok(()) => Ok(()),
         Err(err) => Err(err),
     }
@@ -22,15 +31,37 @@ mod tests {
 
     #[test]
     fn test_delete_file_dir() {
-        let file_path = "test";
-        fs::create_dir(file_path).expect("Failed to create dir");
+        let file_path = "test-dir";
+        fs::create_dir(file_path).expect("Failed to create file");
         assert!(delete_file(file_path).is_err());
-        fs::remove_dir(file_path).expect("Failed to remove dir");
+        fs::remove_dir(file_path).expect("Failed to remove file");
     }
 
     #[test]
     fn test_delete_file_does_not_exist() {
         let file_path = "test.txt";
         assert!(delete_file(file_path).is_err());
+    }
+
+    #[test]
+    fn test_move_file_reg() {
+        let source_file_path = "source.txt";
+        let target_file_path = "target.txt";
+        fs::write(source_file_path, "").expect("Failed to create file");
+        assert!(move_file(source_file_path, target_file_path).is_ok());
+        assert!(fs::metadata(source_file_path).is_err());
+        assert!(fs::metadata(target_file_path).is_ok());
+        fs::remove_file(target_file_path).expect("Failed to remove file");
+    }
+
+    #[test]
+    fn test_move_file_dir() {
+        let source_file_path = "source-dir";
+        let target_file_path = "target-dir";
+        fs::create_dir(source_file_path).expect("Failed to create file");
+        assert!(move_file(source_file_path, target_file_path).is_ok());
+        assert!(fs::metadata(source_file_path).is_err());
+        assert!(fs::metadata(target_file_path).is_ok());
+        fs::remove_dir_all(target_file_path).expect("Failed to remove file");
     }
 }
