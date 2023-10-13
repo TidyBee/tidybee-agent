e3ddfb7c3f335ec0e93b685047229756633ff329
majent4 <99695392+majent4@users.noreply.github.com>
2023-10-13 15:25:33 +0200

    file-man: add basic tests for delete_file function

--- src/file_manager/mod.rs
+++ src/file_manager/mod.rs
@@ -1,13 +1,36 @@
 use std::fs;
 use std::io;
 
-pub fn file_exists(file_path: &str) -> bool {
-    fs::metadata(file_path).is_ok()
-}
-
 pub fn delete_file(file_path: &str) -> Result<(), io::Error> {
     match fs::remove_file(file_path) {
         Ok(()) => Ok(()),
         Err(err) => Err(err),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_delete_file_reg() {
+        let file_path = "test.txt";
+        fs::write(file_path, "").expect("Failed to create file");
+        assert!(delete_file(file_path).is_ok());
+        assert!(fs::metadata(file_path).is_err());
+    }
+
+    #[test]
+    fn test_delete_file_dir() {
+        let file_path = "test";
+        fs::create_dir(file_path).expect("Failed to create dir");
+        assert!(delete_file(file_path).is_err());
+        fs::remove_dir(file_path).expect("Failed to remove dir");
+    }
+
+    #[test]
+    fn test_delete_file_does_not_exist() {
+        let file_path = "test.txt";
+        assert!(delete_file(file_path).is_err());
     }
 }
