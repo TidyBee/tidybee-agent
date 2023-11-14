970af8f89311a15b4f543c60bc635c9441c30740
Ju <julien.hourcadette@live.fr>
2023-11-14 08:06:32 +0100

    agent_infos: clean commentary and useless object wrapper

--- src/agent_infos/agent_infos.rs
+++ src/agent_infos/agent_infos.rs
@@ -1,5 +1,4 @@
 use std::ffi::OsString;
-use std::fmt;
 use gethostname::gethostname;
 use log::info;
 use serde::Deserialize;
@@ -23,21 +22,10 @@ impl Default for AgentVersion {
     }
 }
 
-#[derive(Debug, Default, Deserialize)]
-struct MachineName {
-    name: OsString,
-}
-
-impl fmt::Display for MachineName {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.name.to_string_lossy())
-    }
-}
-
 #[derive(Debug, Default)]
 pub struct AgentInfos {
     agent_version: AgentVersion,
-    machine_name: MachineName,
+    machine_name: OsString,
     process_id: u32,
     uptime: u64,
     watched_directories: Vec<String>,
@@ -64,76 +52,17 @@ impl AgentInfosBuilder {
         self
     }
 
-    // fn agent_version(mut self, version: String) -> Self {
-    //     self.agent_version = AgentVersion { version };
-    //     self
-    // }
-    //
-    // fn machine_name(mut self) -> Self {
-    //     self.machine_name = MachineName { name: gethostname() };
-    //     self
-    // }
-    //
-    // fn process_id(mut self, pid: u32) -> Self {
-    //     self.process_id = ProcessId { pid };
-    //     self
-    // }
-    //
-    // fn process_id_from_system(mut self) -> Self {
-    //     if let Ok(process) = sysinfo::get_current_pid() {
-    //         self.process_id = ProcessId { pid: process.as_u32() };
-    //     }
-    //     self
-    // }
-    //
-    // fn uptime(mut self, time: u64) -> Self {
-    //     self.uptime = Uptime { time };
-    //     self
-    // }
-    //
-    // fn uptime_from_system(mut self) -> Self {
-    //     let system = System::new();
-    //
-    //     self.uptime = Uptime {
-    //         time: system.uptime(),
-    //     };
-    //     self
-    // }
-    //
-    // fn watched_directories(mut self, dirs: Vec<&str>) -> Self {
-    //     self.watched_directories = WatchedDirectories {
-    //         directories: dirs.iter().map(|s| s.to_string()).collect(),
-    //     };
-    //     self
-    // }
-    //
-
-
-
-    //pub struct AgentInfos {
-    //     agent_version: AgentVersion,
-    //     machine_name: MachineName,
-    //     process_id: ProcessId,
-    //     uptime: Uptime,
-    //     watched_directories: WatchedDirectories,
-    //     configuration_directory: ConfigurationDirectory,
-    // }
     pub fn build(self) -> AgentInfos {
         let agent_version: AgentVersion = self.configuration_wrapper
             .bind::<AgentVersion>("agent_config")
             .unwrap_or_default();
-        let process = sysinfo::get_current_pid();
-        let mut dirs: Vec<String> = vec![];
 
-        dirs.push("src".to_string());
         AgentInfos {
             agent_version,
-            machine_name: MachineName{
-                name: gethostname()
-            },
-            process_id: process.unwrap().as_u32(),
+            machine_name: gethostname(),
+            process_id: sysinfo::get_current_pid().unwrap().as_u32(),
             uptime: self.system.uptime(),
-            watched_directories: dirs,
+            watched_directories: vec![],
         }
     }
 }
@@ -143,9 +72,6 @@ impl AgentInfos {
         info!("Voici le status de l'agent et ses configurations :");
         info!("Latest version : {}", self.agent_version.latest_version);
         info!("Minimal version : {}", self.agent_version.minimal_version);
-        info!("Machine name : {}", self.machine_name);
-    }
-
-    pub async fn refresh(self) {
+        info!("Machine name : {:?}", self.machine_name);
     }
 }
