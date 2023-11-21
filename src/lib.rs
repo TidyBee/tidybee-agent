6c7f4f976465b0c02b2a8d109e4e97c4628fa8a5
majent4 <99695392+majent4@users.noreply.github.com>
2023-11-21 13:33:15 +0100

    main: declare pathbuf vecs and pass it to watcher and lister

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod watcher;
 
 use crate::http_server::http_server::HttpServerBuilder;
 use log::{debug, error, info};
+use std::path::PathBuf;
 use std::process;
 use std::thread;
 
@@ -32,10 +33,8 @@ pub async fn run() {
     my_files.init_db().unwrap();
     info!("MyFilesDB sucessfully initialized");
 
-    let directories_list_args: Vec<std::path::PathBuf> =
-        opts.directories_list_args.unwrap_or_default();
-    let directories_watch_args: Vec<std::path::PathBuf> =
-        opts.directories_watch_args.unwrap_or_default();
+    let directories_list_args: Vec<PathBuf> = vec![PathBuf::from(".")];
+    let directories_watch_args: Vec<PathBuf> = vec![PathBuf::from(".")];
     debug!("directories_list_args = {:?}", directories_list_args);
     debug!("directories_watch_args = {:?}", directories_watch_args);
 
@@ -62,12 +61,7 @@ pub async fn run() {
 
     let (sender, receiver) = crossbeam_channel::unbounded();
     let watch_directories_thread: thread::JoinHandle<()> = thread::spawn(move || {
-        watcher::watch_directories(
-            directories_watch_args.clone(),
-            opts.file_extensions_args.clone(),
-            opts.file_types_args.clone(),
-            sender,
-        );
+        watcher::watch_directories(directories_watch_args.clone(), sender);
     });
     info!("File Events Watcher Started");
     for event in receiver {
