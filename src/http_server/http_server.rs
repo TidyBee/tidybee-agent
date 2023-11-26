2add6205b82b72656bc7de88f3624eb3b7f261aa
Ju <julien.hourcadette@live.fr>
2023-11-26 12:27:37 +0100

    http_server: test just import from documentation exemple

--- src/http_server/http_server.rs
+++ src/http_server/http_server.rs
@@ -4,13 +4,14 @@ use crate::configuration_wrapper::ConfigurationWrapper;
 use crate::http_server::routes;
 use crate::my_files;
 use crate::my_files::my_files::{ConfigurationWrapperPresent, ConnectionManagerPresent, Sealed};
-use axum::routing::get;
-use axum::Router;
+use axum::routing::{get, post};
+use axum::{Json, Router};
 use log::{error, info};
 use serde::Deserialize;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use axum::extract::ConnectInfo;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct HttpServerConfig {
@@ -145,4 +146,159 @@ impl HttpServer {
             .await
             .unwrap();
     }
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+   use axum::{
+       body::Body,
+       extract::connect_info::MockConnectInfo,
+       http::{self, Request, StatusCode},
+   };
+   use serde_json::{json, Value};
+   use std::net::SocketAddr;
+   use tokio::net::{TcpListener, TcpStream};
+   use tower::Service; // for `call`
+   use tower::ServiceExt; // for `oneshot` and `ready` // for `collect`
+
+   #[tokio::test]
+   async fn hello_world() {
+       let app = app();
+
+       // `Router` implements `tower::Service<Request<Body>>` so we can
+       // call it like any tower service, no need to run an HTTP server.
+       let response = app
+           .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+           .await
+           .unwrap();
+
+       assert_eq!(response.status(), StatusCode::OK);
+
+       let body = response.into_body().collect().await.unwrap().to_bytes();
+       assert_eq!(&body[..], b"Hello, World!");
+   }
+
+   #[tokio::test]
+   async fn json() {
+       let app = app();
+
+       let response = app
+           .oneshot(
+               Request::builder()
+                   .method(http::Method::POST)
+                   .uri("/json")
+                   .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                   .body(Body::from(
+                       serde_json::to_vec(&json!([1, 2, 3, 4])).unwrap(),
+                   ))
+                   .unwrap(),
+           )
+           .await
+           .unwrap();
+
+       assert_eq!(response.status(), StatusCode::OK);
+
+       let body = response.into_body().collect().await.unwrap().to_bytes();
+       let body: Value = serde_json::from_slice(&body).unwrap();
+       assert_eq!(body, json!({ "data": [1, 2, 3, 4] }));
+   }
+
+   // #[tokio::test]
+   // async fn not_found() {
+   //     let app = app();
+   //
+   //     let response = app
+   //         .oneshot(
+   //             Request::builder()
+   //                 .uri("/does-not-exist")
+   //                 .body(Body::empty())
+   //                 .unwrap(),
+   //         )
+   //         .await
+   //         .unwrap();
+   //
+   //     assert_eq!(response.status(), StatusCode::NOT_FOUND);
+   //     let body = response.into_body().collect().await.unwrap().to_bytes();
+   //     assert!(body.is_empty());
+   // }
+   //
+   // // You can also spawn a server and talk to it like any other HTTP server:
+   // #[tokio::test]
+   // async fn the_real_deal() {
+   //     // TODO(david): convert this to hyper-util when thats published
+   //
+   //     use hyper::client::conn;
+   //
+   //     let listener = TcpListener::bind("0.0.0.0:0").await.unwrap();
+   //     let addr = listener.local_addr().unwrap();
+   //
+   //     tokio::spawn(async move {
+   //         axum::serve(listener, app()).await.unwrap();
+   //     });
+   //
+   //     let target_stream = TcpStream::connect(addr).await.unwrap();
+   //
+   //     let (mut request_sender, connection) = conn::http1::handshake(target_stream).await.unwrap();
+   //
+   //     tokio::spawn(async move { connection.await.unwrap() });
+   //
+   //     let response = request_sender
+   //         .send_request(
+   //             Request::builder()
+   //                 .uri(format!("http://{addr}"))
+   //                 .header("Host", "localhost")
+   //                 .body(Body::empty())
+   //                 .unwrap(),
+   //         )
+   //         .await
+   //         .unwrap();
+   //
+   //     let body = response.into_body().collect().await.unwrap().to_bytes();
+   //     assert_eq!(&body[..], b"Hello, World!");
+   // }
+   //
+   // // You can use `ready()` and `call()` to avoid using `clone()`
+   // // in multiple request
+   // #[tokio::test]
+   // async fn multiple_request() {
+   //     let mut app = app().into_service();
+   //
+   //     let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+   //     let response = ServiceExt::<Request<Body>>::ready(&mut app)
+   //         .await
+   //         .unwrap()
+   //         .call(request)
+   //         .await
+   //         .unwrap();
+   //     assert_eq!(response.status(), StatusCode::OK);
+   //
+   //     let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+   //     let response = ServiceExt::<Request<Body>>::ready(&mut app)
+   //         .await
+   //         .unwrap()
+   //         .call(request)
+   //         .await
+   //         .unwrap();
+   //     assert_eq!(response.status(), StatusCode::OK);
+   // }
+   //
+   // // Here we're calling `/requires-connect-into` which requires `ConnectInfo`
+   // //
+   // // That is normally set with `Router::into_make_service_with_connect_info` but we can't easily
+   // // use that during tests. The solution is instead to set the `MockConnectInfo` layer during
+   // // tests.
+   // #[tokio::test]
+   // async fn with_into_make_service_with_connect_info() {
+   //     let mut app = app()
+   //         .layer(MockConnectInfo(SocketAddr::from(([0, 0, 0, 0], 3000))))
+   //         .into_service();
+   //
+   //     let request = Request::builder()
+   //         .uri("/requires-connect-into")
+   //         .body(Body::empty())
+   //         .unwrap();
+   //     let response = app.ready().await.unwrap().call(request).await.unwrap();
+   //     assert_eq!(response.status(), StatusCode::OK);
+   // }
 }
