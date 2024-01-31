c0083dd421e1774cd4e49f774352aafe1c2b6745
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-01-31 12:15:26 +0000

    TidyScore: Add missnamed rule definition

--- src/tidy_algo/tidy_rules/missnamed.rs
+++ src/tidy_algo/tidy_rules/missnamed.rs
@@ -1,11 +1,38 @@
 use std::collections::HashMap;
 use config::Value;
+use log::warn;
+use regex::Regex;
 
 use crate::{
     file_info::{FileInfo, TidyScore},
     my_files::MyFiles,
 };
 
-pub fn missnamed(file_info: &FileInfo, my_files: &MyFiles, raw_params: HashMap<String, Value>) -> TidyScore {
-    TidyScore::new(false, false, None)
+pub fn missnamed(file_info: &FileInfo, _my_files: &MyFiles, raw_params: HashMap<String, Value>) -> TidyScore {
+    let pattern = raw_params.get("pattern");
+    let mut new_score = match file_info.tidy_score.clone() {
+        Some(score) => score,
+        None => TidyScore::new(false, false, None),
+    };
+
+    if pattern.is_none() {
+        warn!("No pattern provided for missnamed rule");
+        return new_score.clone();
+    } else {
+        let pattern_str = match pattern.unwrap().clone().into_string() {
+            Ok(pattern) => pattern,
+            Err(_) => {
+                warn!("Pattern provided for missnamed rule is not a string");
+                return new_score;
+            }
+        };
+
+        let re = Regex::new(pattern_str.as_str()).unwrap();
+        if re.is_match(&file_info.name) {
+            return new_score;
+        } else {
+            new_score.misnamed = true;
+            return new_score;
+        }
+    }
 }
