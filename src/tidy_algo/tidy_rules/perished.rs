8f2bb25daba7807a8b540310a4b91bbefac1290e
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-01-31 14:37:32 +0000

    TidyRules(perished): Implement perished rule definition

--- src/tidy_algo/tidy_rules/perished.rs
+++ src/tidy_algo/tidy_rules/perished.rs
@@ -1,12 +1,81 @@
-use std::collections::HashMap;
-
 use config::Value;
+use core::time;
+use lazy_static::lazy_static;
+use log::warn;
+use std::collections::HashMap;
 
 use crate::{
     file_info::{FileInfo, TidyScore},
     my_files::MyFiles,
 };
 
-pub fn perished(file_info: &FileInfo, my_files: &MyFiles, raw_params: HashMap<String, Value>) -> TidyScore {
-    TidyScore::new(false, false, None)
+pub fn perished(
+    file_info: &FileInfo,
+    _my_files: &MyFiles,
+    raw_params: HashMap<String, Value>,
+) -> TidyScore {
+    lazy_static! {
+        static ref PERISHED_VALID_UNITS: Vec<&'static str> = vec!["day", "week", "month", "year"];
+    }
+
+    let last_modified = file_info.last_modified.clone();
+
+    let time_string_raw: String = match raw_params.get("max") {
+        Some(time_string) => match time_string.clone().into_string() {
+            Ok(time_string) => time_string,
+            Err(err) => {
+                warn!("{}", err);
+                return TidyScore::new(false, false, None);
+            }
+        },
+        None => {
+            warn!(
+                "No time_string provided for perished rule: {}",
+                file_info.name
+            );
+            return TidyScore::new(false, false, None);
+        }
+    };
+
+    let mut max_time_split = time_string_raw.split(" ");
+    let time_amount = match max_time_split.next() {
+        Some(time_amount) => match time_amount.parse::<u64>() {
+            Ok(time_amount) => time_amount,
+            Err(err) => {
+                warn!("{}", err);
+                return TidyScore::new(false, false, None);
+            }
+        },
+        None => {
+            warn!(
+                "No time_amount provided for perished rule: {}",
+                file_info.name
+            );
+            return TidyScore::new(false, false, None);
+        }
+    };
+    let time_unit = match max_time_split.next() {
+        Some(time_unit) => {
+            if PERISHED_VALID_UNITS.contains(&time_unit) {
+                time_unit
+            } else {
+                warn!(
+                    "Invalid time_unit provided for perished rule: {}",
+                    file_info.name
+                );
+                return TidyScore::new(false, false, None);
+            }
+        }
+        None => {
+            warn!(
+                "No time_unit provided for perished rule: {}",
+                file_info.name
+            );
+            return TidyScore::new(false, false, None);
+        }
+    };
+
+    // TODO: Wait til we decide which time representation we use
+
+    TidyScore::new(false, true, None)
 }
