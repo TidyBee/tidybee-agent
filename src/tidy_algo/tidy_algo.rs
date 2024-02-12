378ba5e584d5a0d0888077e81260326a8cc2fda3
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-02-12 15:40:23 +0000

    TidyAlgo: Add unit tests for loading rules functions

--- src/tidy_algo/tidy_algo.rs
+++ src/tidy_algo/tidy_algo.rs
@@ -6,10 +6,12 @@ use crate::tidy_algo::tidy_rules::perished::perished;
 use config::{Config, ConfigError, File, Value};
 use log::debug;
 use std::collections::HashMap;
+use std::error::Error;
 use std::path;
 
 /// Represents a rule that can be applied to a file
 #[allow(dead_code)]
+#[derive(Debug)]
 pub struct TidyRule {
     name: String,
     log: String,
@@ -19,7 +21,7 @@ pub struct TidyRule {
 }
 
 impl TidyRule {
-    pub fn new(
+    pub const fn new(
         name: String,
         log: String,
         scope: String,
@@ -55,32 +57,133 @@ impl TidyAlgo {
         self.rules.push(Box::new(rule));
     }
 
-    pub fn load_rules_from_file(&mut self, _my_files: &MyFiles, path: path::PathBuf) {
+    /// Load a rule into the ruleset
+    ///
+    /// Returns either the name of the loaded rule or an Error
+    fn load_rule_from_hashmap(&mut self, table: HashMap<String, Value>) -> Result<String, Box<dyn Error>>  {
+        let name = get_string_from_table_safe(&table, "name")?;
+        let log = get_string_from_table_safe(&table, "log")?;
+        let scope = get_string_from_table_safe(&table, "scope")?;
+        let apply_type = get_string_from_table_safe(&table, "type")?;
+        let apply = match apply_type.as_str() {
+            "duplicated" => duplicated,
+            "misnamed" => misnamed,
+            "perished" => perished,
+            fallback => return Err(format!("Could not load rule with type {}", fallback).into())
+        };
+        debug!(
+            "Adding rule {} of type {} that will be logged as {}",
+            name, apply_type, log
+        );
+        self.add_rule(TidyRule::new(name.clone(), log, scope, table, apply));
+        Ok(name)
+    }
+
+    /// Load rules from a file
+    ///
+    /// Returns the number of rules loaded of an error
+    pub fn load_rules_from_file(
+        &mut self,
+        _my_files: &MyFiles,
+        path: path::PathBuf,
+    ) -> Result<usize, Box<dyn std::error::Error>> {
         let rules_config: Result<Config, ConfigError> =
             Config::builder().add_source(File::from(path)).build();
 
         let rules = match rules_config {
             Ok(config) => config.get_array("rules").unwrap(),
-            Err(error) => panic!("Error while loading rules: {error}"),
+            Err(error) => return Err(format!("Error while loading rules: {}", error).into()),
         };
 
         for rule in rules {
-            let rule = rule.into_table().unwrap();
-            let name = rule.get("name").unwrap().clone().into_string().unwrap();
-            let log = rule.get("log").unwrap().clone().into_string().unwrap();
-            let scope = rule.get("scope").unwrap().clone().into_string().unwrap();
-            let apply_type = rule.get("type").unwrap().clone().into_string().unwrap();
-            let apply = match apply_type.as_str() {
-                "duplicated" => duplicated,
-                "misnamed" => misnamed,
-                "perished" => perished,
-                _ => panic!("Unknown rule"),
-            };
-            debug!(
-                "Adding rule {} of type {} that will be logged as {}",
-                name, apply_type, log
-            );
-            self.add_rule(TidyRule::new(name, log, scope, rule, apply));
+            let table = rule.into_table().unwrap();
+            let _ = self.load_rule_from_hashmap(table);
         }
+        Ok(self.rules.len())
     }
 }
+
+/// Helper function the get the key and clone the value or return an Err
+fn get_string_from_table_safe(table: &HashMap<String, Value>, key: &'static str) -> Result<String, ConfigError> {
+    match table.get(key) {
+        Some(value) => {
+            Ok(value.to_string().clone())
+        },
+        None => Err(ConfigError::NotFound(format!("Error while getting the key {}", key)))
+    }
+}
+
+// region: --- TidyAlgo tests
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+    use crate::{
+        configuration::Configuration,
+        my_files::MyFilesBuilder,
+    };
+
+    #[cfg(test)]
+    #[ctor::ctor]
+    fn init() {
+        std::env::set_var("ENV_NAME", "test");
+    }
+
+    #[test]
+    fn create_tidy_algo() {
+        let tidy_algo = TidyAlgo::new();
+        assert_eq!(tidy_algo.rules.len(), 0);
+    }
+
+    #[test]
+    fn add_rule() {
+        let mut tidy_algo = TidyAlgo::new();
+
+        let new_rule = TidyRule::new(
+            "dummy rule".to_string(),
+            "dummy log".to_string(),
+            "dummy scope".to_string(),
+            HashMap::new(),
+            |_, _, _| TidyScore::new(false, false, None),
+        );
+        tidy_algo.add_rule(new_rule);
+
+        assert_eq!(tidy_algo.rules.len(), 1);
+    }
+
+    #[test]
+    fn load_rules_from_file_basic() {
+        let configuration_instance: Configuration = Configuration::init();
+
+        let mut tidy_algo = TidyAlgo::new();
+        let rules_path: PathBuf = [r"tests", r"assets", r"rules_folder", r"basic.yml"]
+            .iter()
+            .collect();
+        let my_files = MyFilesBuilder::new()
+            .configure(configuration_instance.my_files_config)
+            .seal()
+            .build()
+            .unwrap();
+
+        assert_eq!(tidy_algo.load_rules_from_file(&my_files, rules_path).unwrap(), 3);
+    }
+
+    #[test]
+    fn load_rules_from_file_failing() {
+        let configuration_instance: Configuration = Configuration::init();
+
+        let mut tidy_algo = TidyAlgo::new();
+        let rules_path: PathBuf = [r"tests", r"assets", r"rules_folder", r"invalid.yml"]
+            .iter()
+            .collect();
+        let my_files = MyFilesBuilder::new()
+            .configure(configuration_instance.my_files_config)
+            .seal()
+            .build()
+            .unwrap();
+        assert_eq!(tidy_algo.load_rules_from_file(&my_files, rules_path).unwrap(), 0);
+    }
+}
+
+// endregion: --- TidyAlgo tests
