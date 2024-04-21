b535ab59f3f392cffaec462748db5a48723fe9ae
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-04-21 20:00:25 +0100

    change protofile name in build script

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/grpc/proto/tidybee.proto")?;
+    tonic_build::compile_protos("src/grpc/proto/tidybee_events.proto")?;
     Ok(())
 }
